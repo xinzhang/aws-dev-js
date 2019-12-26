cc32902da19661599a13873f2e4420b5d6435894
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-12-26 13:55:22 +1100

    update

--- eks/iam.sh
+++ eks/iam.sh
@@ -1,1 +1,4 @@
 aws iam get-role --role-name "AWSServiceRoleForElasticLoadBalancing" || aws iam create-service-linked-role --aws-service-name "elasticloadbalancing.amazonaws.com"
+
+ aws iam list-instance-profiles --output json | jq -r '.InstanceProfiles[].Roles[].Arn'
+ 
